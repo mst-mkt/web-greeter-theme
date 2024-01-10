1c2fe0043db0c2bea95db696478cb84035d0100d
mst-mkt <s1f102303057@iniad.org>
2024-01-10 21:35:09 +0900

    chore: add packages (stylex)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import styleXVitePlugin from "vite-plugin-stylex";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), styleXVitePlugin(), TanStackRouterVite()],
 });
