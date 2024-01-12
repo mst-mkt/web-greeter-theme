b7618b5c3cb6c0b38c747613a1927ada85846c81
mst-mkt <s1f102303057@iniad.org>
2024-01-12 23:42:37 +0900

    chore: remove @tanstack/router-vite-plugin (because dont use filebase routes)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import styleXVitePlugin from "vite-plugin-stylex";
 
 export default defineConfig({
-  plugins: [react(), styleXVitePlugin(), TanStackRouterVite()],
+  plugins: [react(), styleXVitePlugin()],
 });
