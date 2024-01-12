7d350277c2f3fea62fe74c3ae6223e9535aff99c
mst-mkt <s1f102303057@iniad.org>
2024-01-12 23:48:55 +0900

    change: file name changed (router -> routes)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from '@tanstack/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { router } from './router'
+import { router } from './routes'
 
 // biome-ignore lint/style/noNonNullAssertion: #root is always present in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
