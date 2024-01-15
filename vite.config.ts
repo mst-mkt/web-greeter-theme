e5ddbf5d7438a3c1bc2f95bb26edfefbdb22171a
mst-mkt <s1f102303057@iniad.org>
2024-01-15 21:00:37 +0900

    chore: add basePath in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import styleXVitePlugin from "vite-plugin-stylex";
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import styleXVitePlugin from 'vite-plugin-stylex'
 
 export default defineConfig({
+  base: './',
   plugins: [react(), styleXVitePlugin()],
-});
+})
